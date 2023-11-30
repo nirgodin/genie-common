416d0ecb97688a4dc7f67e2707e4dae88366149f
nir.godin <nir.godin@planckresolution.com>
2023-11-30 08:51:48 +0200

    Created to_datetime util

--- genie_common/utils/__init__.py
+++ genie_common/utils/__init__.py
@@ -1,7 +1,9 @@
+from genie_common.utils.datetime_utils import to_datetime
 from genie_common.utils.dict_utils import safe_nested_get
 from genie_common.utils.error_utils import jsonify_response
 
 __all__ = [
     "safe_nested_get",
-    "jsonify_response"
+    "jsonify_response",
+    "to_datetime"
 ]
---
+++ genie_common/utils/datetime_utils.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from typing import Optional, List
+
+from genie_common.tools import logger
+
+
+def to_datetime(date_time: str, formats: List[str]) -> Optional[datetime]:
+    for format_ in formats:
+        try:
+            return datetime.strptime(date_time, format_)
+        except ValueError:
+            logger.warning(f"Could not parse `{date_time}` to datetime using this format `{format_}`. Trying next.")
+
+    logger.warning(f"Could not parse `{date_time}` to datetime using any of the supplied. Returning None instead.")
