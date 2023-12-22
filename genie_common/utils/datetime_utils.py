690ab8f97e3f9e49d65969db339d59b5474b9e3c
nirgodin <70273595+nirgodin@users.noreply.github.com>
2023-12-22 12:11:04 +0200

    Broaden to_datetime exception catch to avoid redundant exceptions

--- genie_common/utils/datetime_utils.py
+++ genie_common/utils/datetime_utils.py
@@ -8,7 +8,7 @@ def to_datetime(date_time: str, formats: List[str]) -> Optional[datetime]:
     for format_ in formats:
         try:
             return datetime.strptime(date_time, format_)
-        except ValueError:
+        except:
             logger.warning(f"Could not parse `{date_time}` to datetime using this format `{format_}`. Trying next.")
 
     logger.warning(f"Could not parse `{date_time}` to datetime using any of the supplied. Returning None instead.")
