0cb6d556b8cd140d74da508c233ef33c75915892
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-01-29 09:00:15 +0200

    Fix extract_int_from_string bug

--- genie_common/utils/regex_utils.py
+++ genie_common/utils/regex_utils.py
@@ -1,11 +1,13 @@
 import re
-from typing import List
+from typing import List, Optional
 
 
 def search_between_two_characters(start_char: str, end_char: str, text: str) -> List[str]:
     return re.findall(f"{start_char}(.*?){end_char}", text)
 
 
-def extract_int_from_string(s: str) -> int:
+def extract_int_from_string(s: str) -> Optional[int]:
     all_numeric_string = re.sub(r'[^0-9]', '', s)
-    return int(all_numeric_string)
+
+    if all_numeric_string != "":
+        return int(all_numeric_string)
