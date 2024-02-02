83a3480aa9165666ba3b5b9e23d99f97316233be
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-02-02 13:01:47 +0200

    Lowercase strings before computing similarity

--- genie_common/utils/string_utils.py
+++ genie_common/utils/string_utils.py
@@ -2,7 +2,10 @@ from difflib import SequenceMatcher
 
 
 def compute_similarity_score(s1: str, s2: str) -> float:
-    return SequenceMatcher(None, s1, s2).ratio()
+    s1_lower = s1.lower()
+    s2_lower = s2.lower()
+
+    return SequenceMatcher(None, s1_lower, s2_lower).ratio()
 
 
 def string_to_boolean(s: str) -> bool:
