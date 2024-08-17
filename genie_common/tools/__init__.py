f6bda3ee5276f84e2f3a03b2563b01ee9629d730
nirgodin <70273595+nirgodin@users.noreply.github.com>
2024-08-17 14:05:07 +0300

    Improved email sender import

--- genie_common/tools/__init__.py
+++ genie_common/tools/__init__.py
@@ -1,5 +1,6 @@
 from genie_common.tools.aio_pool_executor import AioPoolExecutor
 from genie_common.tools.chunks_generator import ChunksGenerator
+from genie_common.tools.email_sender import EmailSender
 from genie_common.tools.logs import logger
 from genie_common.tools.sync_pool_executor import SyncPoolExecutor
 
@@ -7,5 +8,6 @@ __all__ = [
     "logger",
     "AioPoolExecutor",
     "ChunksGenerator",
+    "EmailSender",
     "SyncPoolExecutor"
 ]
